532ad600f09dc56e6c806b5ae0fa479adffa54fe
justingreco <justin.greco@raleighnc.gov>
2015-05-29 16:47:59 -0400

    added tool icons to tool panel

--- app/toolPanel/toolPanel.js
+++ app/toolPanel/toolPanel.js
@@ -10,11 +10,31 @@ angular.module('imapsNgApp')
 				$scope.toolsChecked = checked;
 			});
 
-			$scope.toolHeaderClick = function (checked) {
-				$rootScope.toolsChecked = !checked;				
-			}
-		}, link: function (scope, element, attr) {
 
+
+			$scope.toolHeaderClick = function (checked) {
+				$rootScope.toolsChecked = !checked;
+			}
+
+			$scope.toolChanged = function (tool) {
+				angular.forEach($scope.tools, function (t, i) {
+					t.highlighted = false;
+				});
+				tool.highlighted = true;
+			}
+
+		}, link: function (scope, element, attr) {
+			scope.tools = [
+				{icon: 'info-sign', title:'Identify', highlighted: true},
+				{icon: 'hand-up', title:'Property Select', highlighted: false},
+				{icon: 'road', title:'Streetview', highlighted: false},
+				{icon: 'resize-horizontal', title:'Measure', highlighted: false},
+				{icon: 'bookmark', title:'Bookmarks', highlighted: false},
+				{icon: 'pencil', title:'Draw', highlighted: false},
+				{icon: 'print', title:'Print', highlighted: false},
+				{icon: 'trash', title:'Clear Map', highlighted: false}
+			];
+			scope.tool = scope.tools[0];
 		}
 	}
 });
