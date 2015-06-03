3698f04e63211bc3301743d6b298ef6b8bca6324
justingreco <justin.greco@raleighnc.gov>
2015-06-03 16:34:56 -0400

    added drawing to measure tool

--- app/toolPanel/measureTool/measureTool.js
+++ app/toolPanel/measureTool/measureTool.js
@@ -4,6 +4,57 @@ angular.module('imapsNgApp')
 		templateUrl: 'toolPanel/measureTool/measureTool.html',
 		restrict: 'E',
 		controller: function ($scope, $filter) {
+			var fill, line, marker, gl;
+
+			var init = function () {
+				require(["esri/toolbars/draw", "esri/layers/GraphicsLayer", "esri/symbols/SimpleMarkerSymbol", "esri/symbols/SimpleFillSymbol", "esri/symbols/SimpleLineSymbol"], function (Draw, GraphicsLayer, SimpleMarkerSymbol, SimpleFillSymbol, SimpleLineSymbol) {
+					gl = new GraphicsLayer();
+					$scope.map.addLayer(gl);
+					fill = new SimpleFillSymbol({
+					  "type": "esriSFS",
+					  "style": "esriSFSSolid",
+						"color": [31,117,254,40],
+					    "outline": {
+					     "type": "esriSLS",
+					     "style": "esriSLSSolid",
+							"color": [31,117,254,200],
+					     "width": 3
+						 }
+					});
+					line = new SimpleLineSymbol({
+						"type": "esriSLS",
+						"style": "esriSLSSolid",
+						"color": [31,117,254,255],
+						"width": 3
+					});
+					marker = new SimpleMarkerSymbol({
+					"type": "esriSMS",
+					 "style": "esriSMSSquare",
+					 "color": [76,115,0,255],
+					 "size": 8,
+					 "angle": 0,
+					 "xoffset": 0,
+					 "yoffset": 0,
+					 "outline":
+					  {
+					  "color": [152,230,0,255],
+					   "width": 1
+					  }
+					});
+
+					draw = new Draw($scope.map);
+					draw.setFillSymbol(fill);
+					draw.setLineSymbol(line);
+					draw.setMarkerSymbol(marker);
+				});
+			}
+
+			$scope.$watch('map', function (map) {
+				if (map) {
+					init();
+				}
+			});
+
 
 			$scope.units = [
 				{label: 'Feet', name: 'FEET', type: 'length', active: true},
@@ -24,7 +75,7 @@ angular.module('imapsNgApp')
 
 			$scope.filterUnits = function (unit) {
 				if ($scope.measureType) {
-					return unit.type === $scope.measureType;
+					return unit.type === $scope.measureType.type;
 				}
 			};
 
@@ -36,12 +87,13 @@ angular.module('imapsNgApp')
 					if (matches.length > 0) {
 						$scope.unit = matches[0];
 					}
+					draw.activate(type.shape);
 				}
 			});
 
 			$scope.unitChanged = function (unit) {
 				var units = $filter('filter')($scope.units, function (u) {
-					return $scope.measureType === u.type;
+					return $scope.measureType.type === u.type;
 				});
 				angular.forEach(units, function (u) {
 					u.active = false;
