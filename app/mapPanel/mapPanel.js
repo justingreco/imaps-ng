f90b2f2228123643a1c3a6cc9aa3d2f3a57aabff
justingreco <justin.greco@raleighnc.gov>
2015-06-05 16:31:10 -0400

    added loading animation

--- app/mapPanel/mapPanel.js
+++ app/mapPanel/mapPanel.js
@@ -117,7 +117,7 @@ angular.module('imapsNgApp')
 					}
 
 					buildSearch();
-
+					$('#loading').remove();
 
 					var home = new HomeButton({map: $scope.map}, 'homeButton').startup();
 				//	var locate = new LocateButton({map: $scope.map}, 'locateButton').startup();
