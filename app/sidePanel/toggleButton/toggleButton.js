fba07a24fc1556afcc4198dd16f479d52a0f13e5
justingreco <justin.greco@raleighnc.gov>
2015-06-05 15:38:26 -0400

    moved toggle button for side panel

--- app/sidePanel/toggleButton/toggleButton.js
+++ app/sidePanel/toggleButton/toggleButton.js
@@ -1,12 +1,32 @@
 angular.module('imapsNgApp')
-.directive('toggleButton', function () {
+.directive('toggleButton', function ($window) {
 	return {
-		templateUrl: 'toggleButton/toggleButton.html',
+		templateUrl: 'sidePanel/toggleButton/toggleButton.html',
 		restrict: 'E',
 		controller: function ($scope, $rootScope) {
-
+			$scope.toggleSidePanel = function () {
+				$rootScope.checked = !$rootScope.checked;
+			};
 		}, link: function (scope, element, attr) {
+			var w = angular.element($window);
+			var moveToggle = function (newValue) {
+				console.log(newValue.h/2 - 52);
+				scope.toggleTop = newValue.h/2 - 52;
+			};
+			scope.getWindowDimensions = function () {
+					return {
+							'h': w.height(),
+							'w': w.width()
+					};
+			};
+			scope.$watch(scope.getWindowDimensions, function (newValue, oldValue) {
+					moveToggle(newValue);
 
+			}, true);
+
+			w.bind('resize', function () {
+					scope.$apply();
+			});
 		}
 	}
 });
