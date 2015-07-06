76afe9694059372641238ba3df889b6af9898d9b
justingreco <justin.greco@raleighnc.gov>
2015-07-06 13:15:30 -0400

    finished address search

--- app/directives/sidePanel/locationSearch/addressSearch/addressSearch.js
+++ app/directives/sidePanel/locationSearch/addressSearch/addressSearch.js
@@ -3,8 +3,14 @@ angular.module('imapsNgApp')
 	return {
 		templateUrl: 'directives/sidePanel/locationSearch/addressSearch/addressSearch.html',
 		restrict: 'EA',
-		controller: function ($scope, $rootScope) {
-
+		controller: function ($scope, locationFactory) {
+			$scope.geocodeAddress = function () {
+				locationFactory.geocodeAddress($scope.streetAddress).then(function (response) {
+					if (response.candidates.length > 0) {
+						$scope.map.centerAndZoom(response.candidates[0].location, 10);
+					}
+				});
+			};
 		},
 		link: function (scope, element, attrs) {
 			// scope.searches = [{label: 'For Property', value: 'property'}, {label: 'For Location', value: 'location'}];
