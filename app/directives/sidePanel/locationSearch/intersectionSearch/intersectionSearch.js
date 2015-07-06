2473ac3e3072a5ed380c5f51b43c2d1992639b0b
justingreco <justin.greco@raleighnc.gov>
2015-07-06 13:12:14 -0400

    finished intersection search

--- app/directives/sidePanel/locationSearch/intersectionSearch/intersectionSearch.js
+++ app/directives/sidePanel/locationSearch/intersectionSearch/intersectionSearch.js
@@ -3,8 +3,25 @@ angular.module('imapsNgApp')
 	return {
 		templateUrl: 'directives/sidePanel/locationSearch/intersectionSearch/intersectionSearch.html',
 		restrict: 'EA',
-		controller: function ($scope, $rootScope) {
-
+		controller: function ($scope, $http, locationFactory) {
+			$scope.primaryStreet = null;
+			$scope.primaryStreets = function(street){
+		        return locationFactory.getStreets(street).then(function (response) {
+		        	return response.features;
+		        });
+		    };
+		    $scope.primaryStreetSelected = function ($item, $model, $label) {
+				locationFactory.getIntersectingStreets($item.geometry).then(function (response) {
+					$scope.intersectingStreets = response.features;
+				});
+		    };
+		    $scope.intersectionSelected = function () {
+				locationFactory.geocodeAddress($scope.primaryStreet.attributes.CARTONAME + ' & ' +$scope.intersectingStreet.attributes.CARTONAME).then(function (response) {
+					if (response.candidates.length > 0) {
+						$scope.map.centerAndZoom(response.candidates[0].location, 10);
+					}
+				});
+		    };
 		},
 		link: function (scope, element, attrs) {
 			// scope.searches = [{label: 'For Property', value: 'property'}, {label: 'For Location', value: 'location'}];
--- app/factories/location.js
+++ app/factories/location.js
@@ -1,6 +1,6 @@
 angular.module('imapsNgApp').factory('locationFactory', ['$http', '$q', function($http, $q){
 
-	var service = {getSubdivision:getSubdivision},
+	var service = {getSubdivision:getSubdivision, getStreets:getStreets, getIntersectingStreets:getIntersectingStreets, geocodeAddress:geocodeAddress},
 		baseUrl = "http://maps.raleighnc.gov/arcgis/rest/services/Parcels/MapServer/exts/PropertySOE/",
 		serviceUrl = "http://maps.raleighnc.gov/arcgis/rest/services/Services/ServicesIMaps/MapServer",
 		propertyLayer = "http://maps.raleighnc.gov/arcgis/rest/services/Parcels/MapServer";
@@ -19,5 +19,50 @@ angular.module('imapsNgApp').factory('locationFactory', ['$http', '$q', function
 			}		
 		}).success(deferred.resolve);
 		return deferred.promise;
-	}
+	};
+	function getStreets (street) {
+		var deferred = $q.defer();
+		$http({
+			method: 'GET',
+			url: 'https://maps.raleighnc.gov/arcgis/rest/services/StreetsDissolved/MapServer/0/query',
+			params: {
+				where: "CARTONAME LIKE '" + street.toUpperCase() + "%'",
+				orderByFields: 'CARTONAME',
+				outFields: 'CARTONAME',
+				returnGeometry: true,
+				f: "json"
+			}		
+		}).success(deferred.resolve);
+		return deferred.promise;
+	};	
+	function getIntersectingStreets (geom) {
+		var deferred = $q.defer();
+		$http({
+			method: 'POST',
+			url: 'https://maps.raleighnc.gov/arcgis/rest/services/StreetsDissolved/MapServer/0/query',
+			data: $.param({
+				geometry: JSON.stringify(geom),
+				geometryType: 'esriGeometryPolyline',
+				returnGeometry: false,
+				outFields: 'CARTONAME',
+				orderByFields: 'CARTONAME',
+				f: "json"
+			}),
+        	headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+		}).success(deferred.resolve);
+		return deferred.promise;
+	};
+	function geocodeAddress (address) {
+		var deferred = $q.defer();
+		$http({
+			method: 'GET',
+			url: 'https://maps.raleighnc.gov/arcgis/rest/services/Locators/Locator/GeocodeServer/findAddressCandidates',
+			params: {
+				'Single Line Input': address,
+				returnGeometry: true,
+				f: "json"
+			}		
+		}).success(deferred.resolve);
+		return deferred.promise;
+	};			
 }]);
