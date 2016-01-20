93f16811dd7b7b632236293d41fe630f63b3d6ee
justingreco <justin.greco@raleighnc.gov>
2016-01-20 09:31:50 -0500

    updated radio button groups to new version of angular-ui bootstrap

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -55,4 +55,14 @@ angular
         ctrl.$parsers.push(inputValue);
       }
     };
-});
+}).config(['$tooltipProvider', function ($tooltipProvider) {
+  var parser = new UAParser();
+  var result = parser.getResult();
+  var touch = result.device && (result.device.type === 'tablet'  || result.device.type === 'mobile');
+  if (touch) {
+    var options = {
+      trigger = 'dontTrigger';
+    };
+    $tooltipProvider.options(options);
+  }
+}]);
