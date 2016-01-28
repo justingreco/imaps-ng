9b9328a4abc5a34e2b9fe8ac6852b0a3d7fb8cb9
justingreco <justin.greco@raleighnc.gov>
2016-01-28 14:01:19 -0500

    add marker for address search

--- app/directives/sidePanel/locationSearch/addressSearch/addressSearch.js
+++ app/directives/sidePanel/locationSearch/addressSearch/addressSearch.js
@@ -5,11 +5,30 @@ angular.module('imapsNgApp')
 		restrict: 'EA',
 		controller: function ($scope, locationFactory) {
 			$scope.geocodeAddress = function () {
-				locationFactory.geocodeAddress($scope.streetAddress).then(function (response) {
-					if (response.candidates.length > 0) {
-						$scope.map.centerAndZoom(response.candidates[0].location, 10);
-					}
-				});
+		        require([
+		          "esri/geometry/Point", "esri/layers/GraphicsLayer", "esri/graphic", "esri/symbols/PictureMarkerSymbol"
+		        ], function(Point, GraphicsLayer, Graphic, PictureMarkerSymbol) {
+			        locationFactory.geocodeAddress($scope.streetAddress).then(function (response) {
+						if (response.candidates.length > 0) {
+					          var gl = $scope.map.getLayer('locationGraphics');
+					          var g = null;
+					          if (!gl) {
+					            gl = new GraphicsLayer({id: 'locationGraphics'});
+					            $scope.map.addLayer(gl);
+					          }
+					          gl.clear();
+					          var symbol =  new PictureMarkerSymbol({
+					            "url":"images/esriGreenPin16x26.png",
+					            "height":26,
+					            "width":16,
+					            "type":"esriPMS"
+					          });							  
+					          gl.add(new Graphic(new Point({"x": response.candidates[0].location.x, "y": response.candidates[0].location.y, "spatialReference": {"wkid": $scope.map.spatialReference.latestWkid } }), symbol));
+					          
+					          $scope.map.centerAndZoom(response.candidates[0].location, 11);							
+						}
+					});
+		        });					
 			};
 		},
 		link: function (scope, element, attrs) {
