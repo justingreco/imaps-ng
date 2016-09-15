66b958d7a3ddc3778e3069e3590194066f7c8132
justingreco <justin.greco@raleighnc.gov>
2016-09-15 10:40:18 -0400

    change getRealEstate to POST

--- app/factories/property.js
+++ app/factories/property.js
@@ -28,7 +28,7 @@ angular.module('imapsNgApp').factory('property', ['$http', '$q', function($http,
 			method: 'POST',
 			url: baseUrl + "properties/" + type,
 			data: $.param({
-				values: values//JSON.stringify(values)
+				values: JSON.parse(values)//JSON.stringify(values)
 			}),
         headers: {'Content-Type': 'application/x-www-form-urlencoded'}
 		}).success(deferred.resolve);
